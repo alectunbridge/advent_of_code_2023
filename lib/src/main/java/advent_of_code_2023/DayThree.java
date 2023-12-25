b583448a493cb173d0fc34a495fc98cff0bf6171
alec.tunbridge <alec.tunbridge@gmail.com>
2023-12-25 20:52:07 +0000

    Day03 part2

--- lib/src/main/java/advent_of_code_2023/DayThree.java
+++ lib/src/main/java/advent_of_code_2023/DayThree.java
@@ -1,8 +1,5 @@
 package advent_of_code_2023;
 
-import com.google.common.base.MoreObjects;
-
-import javax.swing.plaf.synth.SynthButtonUI;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,13 +24,13 @@ public class DayThree {
         }
 
         for (Symbol symbol : symbols) {
-            findNumbersNextToCoordinate(symbol.lineIndex,symbol.start);
+            findNumbersNextToCoordinate(symbol.lineIndex, symbol.start);
         }
         return validNumbers.stream().mapToInt(PartNumber::number).sum();
     }
 
     private void findNumbersNextToCoordinate(int lineIndex, int start) {
-        for( int lineDelta = -1; lineDelta < 2; lineDelta++ ) {
+        for (int lineDelta = -1; lineDelta < 2; lineDelta++) {
             for (PartNumber partNumber : numbers[lineIndex + lineDelta]) {
                 if (Math.abs(partNumber.start - start) <= 1 || Math.abs(partNumber.end - start) <= 1) {
                     validNumbers.add(partNumber);
@@ -45,22 +42,59 @@ public class DayThree {
     private void parseLineIntoNumbersAndSymbols(int lineIndex, String line) {
         Pattern numberPattern = Pattern.compile("\\d+");
         Matcher numberMatcher = numberPattern.matcher(line);
-        while(numberMatcher.find()){
-            PartNumber partNumber = new PartNumber(Integer.parseInt(numberMatcher.group(0)), lineIndex, numberMatcher.start(), numberMatcher.end()-1);
+        while (numberMatcher.find()) {
+            PartNumber partNumber = new PartNumber(Integer.parseInt(numberMatcher.group(0)), lineIndex, numberMatcher.start(), numberMatcher.end() - 1);
             numbers[lineIndex].add(partNumber);
         }
-        
+
         Pattern symbolPattern = Pattern.compile("[^\\d.]");
         Matcher symbolMatcher = symbolPattern.matcher(line);
-        while(symbolMatcher.find()){
-            Symbol symbol = new Symbol(lineIndex, symbolMatcher.start());
+        while (symbolMatcher.find()) {
+            Symbol symbol = new Symbol(symbolMatcher.group(0), lineIndex, symbolMatcher.start());
             symbols.add(symbol);
         }
-        
-        
+
+
     }
 
-    private record PartNumber(int number, int lineIndex, int start, int end) {}
+    public long part2() {
+        for (int lineIndex = 0; lineIndex < input.size(); lineIndex++) {
+            numbers[lineIndex] = new HashSet<>();
+            String line = input.get(lineIndex);
+            parseLineIntoNumbersAndSymbols(lineIndex, line);
+        }
+        long total = 0;
+        for (Symbol symbol : symbols) {
+            if (symbol.isGear()) {
+                total += findGear(symbol.lineIndex, symbol.start);
+            }
+        }
 
-    private record Symbol(int lineIndex, int start){}
+
+        return total;
+    }
+
+    private int findGear(int lineIndex, int start) {
+        Set<PartNumber> gears = new HashSet<>();
+        for (int lineDelta = -1; lineDelta < 2; lineDelta++) {
+            for (PartNumber partNumber : numbers[lineIndex + lineDelta]) {
+                if (Math.abs(partNumber.start - start) <= 1 || Math.abs(partNumber.end - start) <= 1) {
+                    gears.add(partNumber);
+                }
+            }
+        }
+        if (gears.size() == 2) {
+            return gears.stream().mapToInt(PartNumber::number).reduce(1, (a, b) -> a * b);
+        }
+        return 0;
+    }
+
+    private record PartNumber(int number, int lineIndex, int start, int end) {
+    }
+
+    private record Symbol(String character, int lineIndex, int start) {
+        public boolean isGear() {
+            return "*".equals(character);
+        }
+    }
 }
--- lib/src/test/java/advent_of_code_2023/DayThreeTest.java
+++ lib/src/test/java/advent_of_code_2023/DayThreeTest.java
@@ -2,6 +2,7 @@ package advent_of_code_2023;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,15 +23,38 @@ public class DayThreeTest {
                 "...$.*....",
                 ".664.598..")
         );
-        
+
         assertThat(dayThree.part1()).isEqualTo(4361);
     }
-    
+
     @Test
     void part1() {
         DayThree dayThree = new DayThree(Utils.readInputLinesFromFile("day03.txt")
         );
-        
+
         assertThat(dayThree.part1()).isEqualTo(4361);
+    }
+
+    @Test
+    void part2Example() {
+        assertThat(new DayThree(Arrays.asList(
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598..")
+        ).part2()).isEqualTo(467835);
+    }
+
+    @Test
+    void part2() {
+        DayThree dayThree = new DayThree(Utils.readInputLinesFromFile("day03.txt"));
+
+        assertThat(dayThree.part2()).isEqualTo(89471771);
     }
 }
