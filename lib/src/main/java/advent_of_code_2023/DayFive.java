328ac3bed4ac451bf6d3b3388663d0c34e8d061f
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2023-12-30 16:38:32 +0000

    DayFive part 2 ouch

--- lib/src/main/java/advent_of_code_2023/DayFive.java
+++ lib/src/main/java/advent_of_code_2023/DayFive.java
@@ -1,8 +1,8 @@
 package advent_of_code_2023;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.google.common.base.MoreObjects;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DayFive {
@@ -42,7 +42,7 @@ public class DayFive {
                     }
                 }
             }
-            if(seed < closestSeed) {
+            if (seed < closestSeed) {
                 closestSeed = seed;
             }
         }
@@ -50,25 +50,125 @@ public class DayFive {
         return closestSeed;
     }
 
-    private class SeedRange {
-
-        private final long sourceStart;
-        private final long sourceEnd;
-        private final long offset;
-
-        public SeedRange(String[] params) {
-            long destinationStart = Long.parseLong(params[0]);
-            sourceStart = Long.parseLong(params[1]);
-            long range = Long.parseLong(params[2]);
-            sourceEnd = sourceStart + range;
-            offset = destinationStart - sourceStart;
+    public long part2() {
+        String[] seedString = input.getFirst().replace("seeds: ", "").split(" ");
+        List<SeedRange> startRanges = new ArrayList<>();
+        for (int i = 0; i <= seedString.length / 2; i += 2) {
+            long start = Long.parseLong(seedString[i]);
+            long rangeSize = Long.parseLong(seedString[i + 1]);
+            startRanges.add(new SeedRange(start, start + rangeSize - 1));
         }
 
-        public Long mapSeed(Long input) {
-            if (input >= sourceStart && input <= sourceEnd) {
-                return input + offset;
+        for (int lineIndex = 2; lineIndex < input.size(); lineIndex++) {
+            String currentLine = input.get(lineIndex);
+            if ("".equals(currentLine)) {
+                maps.getLast().sort(Comparator.comparingLong(SeedRange::getSourceStart));
+
+                SeedRange firstRange = maps.getLast().getFirst();
+                if (firstRange.getSourceStart() != 0) {
+                    maps.getLast().addFirst(new SeedRange(0, firstRange.getSourceStart() - 1, firstRange.getSourceStart()));
+                }
+
+                long lastRangeStart = maps.getLast().getLast().getSourceEnd() + 1;
+                maps.getLast().add(new SeedRange(lastRangeStart, lastRangeStart, Long.MAX_VALUE - lastRangeStart));
+            } else if (currentLine.contains(":")) {
+                maps.add(new ArrayList<>());
+            } else {
+                maps.getLast().add(new SeedRange(currentLine.split(" ")));
             }
-            return null;
         }
+
+        List<SeedRange> inputRanges = startRanges;
+        for (List<SeedRange> map : maps) {
+            inputRanges = DayFive.mapInputRangesToOutputRanges(inputRanges, map);
+        }
+
+        return inputRanges.stream().mapToLong(SeedRange::getDestinationStart).min().getAsLong();
+    }
+
+    private static List<SeedRange> mapInputRangesToOutputRanges(List<SeedRange> inputRanges, List<SeedRange> mappingRanges) {
+        List<SeedRange> outputRanges = new ArrayList<>();
+        for (SeedRange inputRange : inputRanges) {
+            for (SeedRange mappingRange : mappingRanges) {
+                SeedRange overlap = inputRange.destinationOverlapWithSource(mappingRange);
+                if(overlap != null){
+                    outputRanges.add(overlap);
+                }
+            }
+        }
+        return outputRanges;
+    }
+
+
+}
+
+class SeedRange {
+
+    Long sourceStart;
+    Long sourceEnd;
+    Long offset;
+    Long destinationStart;
+    Long destinationEnd;
+
+    public Long getSourceStart() {
+        return sourceStart;
+    }
+
+    public Long getSourceEnd() {
+        return sourceEnd;
+    }
+
+    public Long getOffset() {
+        return offset;
+    }
+
+    public Long getDestinationStart() {
+        return destinationStart;
+    }
+
+    public Long getDestinationEnd() {
+        return destinationEnd;
+    }
+
+    public SeedRange(String[] params) {
+        destinationStart = Long.parseLong(params[0]);
+        sourceStart = Long.parseLong(params[1]);
+        long range = Long.parseLong(params[2]);
+        sourceEnd = sourceStart + range - 1;
+        destinationEnd = destinationStart + range;
+        offset = destinationStart - sourceStart;
+    }
+
+    public SeedRange(long destinationStart, long destinationEnd) {
+        this.destinationStart = destinationStart;
+        this.destinationEnd = destinationEnd;
+        this.offset = null;
+        this.sourceStart = null;
+        this.sourceEnd = null;
+    }
+
+    public SeedRange(long sourceStart, long sourceEnd, long range) {
+        this.sourceStart = sourceStart;
+        this.sourceEnd = sourceStart + range - 1;
+        this.destinationStart = sourceStart;
+        this.destinationEnd = sourceStart + range - 1;
+        this.offset = 0L;
+    }
+
+    public Long mapSeed(Long input) {
+        if (input >= sourceStart && input <= sourceEnd) {
+            return input + offset;
+        }
+        return null;
+    }
+
+    public SeedRange destinationOverlapWithSource(SeedRange nextRange) {
+        long maxStart = Long.max(this.destinationStart, nextRange.sourceStart);
+        long minEnd = Long.min(this.destinationEnd, nextRange.sourceEnd);
+        if (maxStart <= minEnd) {
+            return new SeedRange(maxStart + nextRange.offset, minEnd + nextRange.offset);
+        }
+
+        return null;
     }
 }
--- lib/src/test/java/advent_of_code_2023/DayFiveTest.java
+++ lib/src/test/java/advent_of_code_2023/DayFiveTest.java
@@ -43,10 +43,53 @@ public class DayFiveTest {
                 "",
                 "humidity-to-location map:",
                 "60 56 37",
-                "56 93 4"
+                "56 93 4",
+                ""
         ));
 
         assertThat(dayFive.part1()).isEqualTo(35);
+    }
+    
+    @Test
+    void part2Example() {
+        DayFive dayFive = new DayFive(Arrays.asList(
+                "seeds: 79 14 55 13",
+                "",
+                "seed-to-soil map:",
+                "50 98 2",
+                "52 50 48",
+                "",
+                "soil-to-fertilizer map:",
+                "0 15 37",
+                "37 52 2",
+                "39 0 15",
+                "",
+                "fertilizer-to-water map:",
+                "49 53 8",
+                "0 11 42",
+                "42 0 7",
+                "57 7 4",
+                "",
+                "water-to-light map:",
+                "88 18 7",
+                "18 25 70",
+                "",
+                "light-to-temperature map:",
+                "45 77 23",
+                "81 45 19",
+                "68 64 13",
+                "",
+                "temperature-to-humidity map:",
+                "0 69 1",
+                "1 0 69",
+                "",
+                "humidity-to-location map:",
+                "60 56 37",
+                "56 93 4",
+                ""
+        ));
+
+        assertThat(dayFive.part2()).isEqualTo(46);
     }
 
 
@@ -54,4 +97,25 @@ public class DayFiveTest {
     void part1() {
         assertThat(new DayFive(Utils.readInputLinesFromFile("day05.txt")).part1()).isEqualTo(836040384);
     }
+    @Test
+    void part2() {
+        assertThat(new DayFive(Utils.readInputLinesFromFile("day05.txt")).part2()).isEqualTo(10834440);
+    }
+
+    @Test
+    void overlapOccurs() {
+        SeedRange startRange = new SeedRange(79,92);
+        SeedRange nextRange = new SeedRange(new String[]{"52", "50", "48"});
+        SeedRange outputRange = startRange.destinationOverlapWithSource(nextRange);
+        assertThat(outputRange.destinationStart).isEqualTo(81);
+        assertThat(outputRange.destinationEnd).isEqualTo(94);
+    }
+    @Test
+    void overlapDoesNotOccur() {
+        SeedRange startRange = new SeedRange(79,92);
+        SeedRange nextRange = new SeedRange(new String[]{"50", "98", "2"});
+        SeedRange outputRange = startRange.destinationOverlapWithSource(nextRange);
+        assertThat(outputRange).isNull();
+    }
+    
 }
